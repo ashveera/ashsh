1abc9bbec1bd52c5a670cf85c58ed416e6564b63
ashveera <32736662+ashveera@users.noreply.github.com>
2025-01-15 18:49:13 +0200

    Update posts.js

--- api/posts.js
+++ api/posts.js
@@ -26,7 +26,8 @@ module.exports = async (req, res) => {
                 const imageResponse = await fetch(imageUrl);
 
                 if (!imageResponse.ok) {
-                    throw new Error(Failed to fetch image: ${imageResponse.statusText});
+                  throw new Error(Failed to fetch image: ${imageResponse.statusText});
+
                 }
 
                 const imageBuffer = await imageResponse.buffer();
